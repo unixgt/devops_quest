b63569f996881781af4643256ee29f6d56c594c8
unixgt <maxdevops38@gmail.com>
2026-03-10 18:51:15 +0500

    system investigation

---
+++ scripts/system_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "========================="
+echo "System investigation"
+echo "Username $(whoami)"
+echo "Current directory: $(pwd)"
+echo "Date and time: $(date)"
+echo "System up time $(uptime -p)"
+echo "========================"
